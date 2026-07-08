71c938ccde44bdc9e595dc6a7622e4d600a32859
Alexey Perestoronin <Alexey.Perestoronin@yandex.ru>
2026-07-08 20:24:35 +0600

    Кинематический-Верстак V3: проектирование-17

--- Projects/МАСТЕРСКАЯ/Кинематический-Верстак/V3/automated.py
+++ Projects/МАСТЕРСКАЯ/Кинематический-Верстак/V3/automated.py
@@ -40,17 +40,29 @@ def make_doc(ctx):
         for reference_component in save_folder.iterdir():
             reference_component.unlink(missing_ok=True)
 
-        steel_sheet_8 = []
+        steel_sheet_8mm = []
+        steel_sheet_6mm = []
+        steel_tube_150_150_5mm = []
+        steel_tube_100_50_5mm = []
         undefined = []
 
         for (reference_body, quantity, reference_component, step_path) in save_paths_and_bodies:
             component_full_name = step_path.name
 
-            try_detect = lambda expression: bool(re.match(expression, component_full_name))
+            try_detect = lambda expression: bool(re.match(f"Верстак {expression}", component_full_name))
 
             # steel_sheet_8
-            if try_detect(r"NOT_IMPLEMENTED") or try_detect(r".+ПП-.+"):
-                steel_sheet_8.append([component_full_name, quantity * 2])
+            if try_detect(r"каркас столешница"):
+                steel_sheet_8mm.append([component_full_name, quantity])
+            # steel_sheet_6
+            elif try_detect(r"каркас юбка-.+") or try_detect(r"ножки .+") or try_detect(r".+ рж-.+"):
+                steel_sheet_6mm.append([component_full_name, quantity])
+            # steel_tube_150_150_5mm
+            elif try_detect(r"каркас колонна-опорная"):
+                steel_tube_150_150_5mm.append([component_full_name, quantity])
+            # steel_tube_100_50_5mm
+            elif try_detect(r"каркас колонна-ножек-.+"):
+                steel_tube_100_50_5mm.append([component_full_name, quantity])
             else:
                 utils.warning.log_line(f"detected DOC-unclassified step-file: {step_path}")
                 undefined.append([component_full_name, quantity])
@@ -68,13 +80,28 @@ def make_doc(ctx):
             "Траектория реза задается относительно контура детали следующим образом:",
             "- для сквозных отверстий, пазов и иных внутренних элементов, рез выполняется по внутреннему контуру (материал удаляется изнутри контура);",
             "- для наружного контура детали (отрезка заготовки), рез выполняется по внешнему контуру (материал удаляется снаружи контура).",
-            
+            "",
             "❗В случае, если фактические параметры металлических заготовок будут отличаться от заданных, прошу сообщить отдельно для внесения корректировок в проект изделия!",
             "",
             "## Лист стальной горячекатанный 8мм",
             "[Справочная ссылка для материала](https://купитьметалл.рф/product/list-gk-8-st3sp-ps-5)",
             "",
-            make_md_tabla(steel_sheet_8),
+            make_md_tabla(steel_sheet_8mm),
+            "",
+            "## Лист стальной горячекатанный 6мм",
+            "[Справочная ссылка для материала](https://купитьметалл.рф/product/list-gk-6-st3sp-ps-5)",
+            "",
+            make_md_tabla(steel_sheet_6mm),
+            "",
+            "## Труба стальная прокатная 150-150-5мм",
+            "[Справочная ссылка для материала](https://купитьметалл.рф/product/truba-kvadratnaya-150x150x5)",
+            "",
+            make_md_tabla(steel_tube_150_150_5mm),
+            "",
+            "## Труба стальная прокатная 100-50-5мм",
+            "[Справочная ссылка для материала](https://купитьметалл.рф/product/truba-pryamougol-100x50x5)",
+            "",
+            make_md_tabla(steel_tube_100_50_5mm),
         ]
         
         if len(undefined) > 0:
