f92fa73eef333c5f078d868979d9cccd50c97450
Alexey Perestoronin <Alexey.Perestoronin@yandex.ru>
2026-07-14 20:20:51 +0600

    improve scripts

--- Projects/МАСТЕРСКАЯ/automated.py
+++ Projects/МАСТЕРСКАЯ/automated.py
@@ -3,11 +3,8 @@ import shutil
 import invoke
 import pathlib
 
-from pyswx.api.swconst.enumerations import SWDocumentTypesE
-
 import utils
 import utils.doc_creator
-import check
 
 
 @invoke.task()
@@ -22,27 +19,24 @@ def make_doc_for_workbench_1000_600(ctx):
     """
     project_name = 'Верстак-Dim1000x600x50'
     project_path = pathlib.Path(__file__).with_name(f'{project_name}.SLDPRT')
-    check.project_naming(ctx, project_path)
-    root_model = utils.open_document(project_path, SWDocumentTypesE.SW_DOC_PART).root_model
+    doc_save_folder = project_path.with_name(f'{project_name}-DOC')
 
     unique_bodies_manager = utils.UniqueBodiesManager()
-    unique_bodies_manager.add_from_model(root_model)
-
-    save_folder = project_path.with_name(f'{project_name}-DOC')
+    unique_bodies_manager.add_from_project(project_path)
     saving_groups = utils.prepare_saving_groups_2(unique_bodies_manager.unique_bodies)
 
     execute = True
     if execute:
-        shutil.rmtree(save_folder, ignore_errors=True)
+        shutil.rmtree(doc_save_folder, ignore_errors=True)
 
         td_preparator = utils.doc_creator.CNCLaserCuttingDocCreator.TableDataPreparator(
-            saving_groups, save_folder, lambda expression, component_full_name: bool(re.match(f"{project_name} {expression}", component_full_name)))
+            saving_groups, doc_save_folder, lambda expression, component_full_name: bool(re.match(f"{project_name} {expression}", component_full_name)))
         td_preparator.unused([r"столешница"])
         utils.doc_creator.CNCLaserCuttingDocCreator(project_name) \
             .add_table('Лист стальной горячекатанный 6мм', td_preparator.prepare(True, True, [r".+-6мм.+"], quantity_expression=lambda x :  10), 'https://купитьметалл.рф/product/list-gk-6-st3sp-ps-5') \
             .add_table('Лист стальной горячекатанный 4мм', td_preparator.prepare(True, True, [r"крепёжный-уголок"], quantity_expression=lambda x :  16), 'https://купитьметалл.рф/product/list-gk-4-st3sp-ps-5') \
             .add_table('Не учтённые элементы', td_preparator.unclassified()) \
-            .create(save_folder)
+            .create(doc_save_folder)
 
 
 collection = invoke.Collection()
