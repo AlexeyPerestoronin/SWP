7738ea06b7b6a4ca7a3c0e976805108c2829466c
Alexey Perestoronin <Alexey.Perestoronin@yandex.ru>
2026-07-21 18:24:06 +0600

    Держатель-Сварочной-Горелки: документирование-7

--- Projects/МАСТЕРСКАЯ/Держатель-Для-Сварочной-Горелки/automated.py
+++ Projects/МАСТЕРСКАЯ/Держатель-Для-Сварочной-Горелки/automated.py
@@ -21,7 +21,7 @@ def parse_saving_groups(ctx):
     if not hasattr(ctx, 'saving_groups'):
         unique_bodies_manager = utils.UniqueBodiesManager()
         unique_bodies_manager.add_from_project(PROJECT_PATH)
-        saving_groups = utils.prepare_saving_groups_2(unique_bodies_manager.unique_bodies)
+        saving_groups = utils.prepare_saving_groups(unique_bodies_manager.unique_bodies)
         setattr(ctx, 'saving_groups', saving_groups)
 
 
@@ -34,11 +34,9 @@ def prepare_manufacturing_doc(ctx):
         shutil.rmtree(manufacturing_doc_folder, ignore_errors=True)
 
         td_preparator = utils.doc_creator.CNCLaserCuttingDocCreator.TableDataPreparator(
-            saving_groups, manufacturing_doc_folder, lambda expression, component_full_name: bool(re.match(f"{expression}", component_full_name)))
-        td_preparator.unused([f"({PROJECT_NAME} магнит|hex nut .+|hex bolt .+)"])
+            saving_groups, manufacturing_doc_folder, lambda expression, component_full_name: bool(re.match(f"{PROJECT_NAME} {expression}", component_full_name)))
         utils.doc_creator.CNCLaserCuttingDocCreator(PROJECT_NAME) \
-            .add_4mm_steel_sheet_table(td_preparator.prepare(True, True, [f"{PROJECT_NAME} (основание|держатель) .+"], quantity_expression=lambda x :  x * 2)) \
-            .add_unclassified_table(td_preparator.unclassified()) \
+            .add_4mm_steel_sheet_table(td_preparator.prepare([f"(основание|держатель) .+"], step=True, dxf=True, quantity_expression=lambda x :  x * 2)) \
             .create(manufacturing_doc_folder)
 
 
@@ -68,7 +66,10 @@ def convert_doc_to_zip(ctx):
 @utils.sw_task(doc_string=f"Make complex documentation for '{PROJECT_NAME}'",
                pre=[clear_doc_folder, parse_saving_groups, prepare_manufacturing_doc, prepare_assembling_doc, convert_doc_to_zip])
 def make_doc(ctx):
-    pass
+    saving_groups = getattr(ctx, 'saving_groups')
+    for saving_group in saving_groups:
+        if saving_group.mark is None:
+            utils.logger.error.log_line(f"detected unclassified/unused elements '{saving_group.save_file_name}'")
 
 
 collection = invoke.Collection()
