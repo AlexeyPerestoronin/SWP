7f0a10f2986c42a61eab0e6c280cc2eb7dcb1957
Alexey Perestoronin <Alexey.Perestoronin@yandex.ru>
2026-07-30 19:57:47 +0600

    fix state

--- Projects/МАСТЕРСКАЯ/Верстак-Dim1000x600x50/automated.py
+++ Projects/МАСТЕРСКАЯ/Верстак-Dim1000x600x50/automated.py
@@ -1,15 +1,15 @@
 import re
-import invoke
 import shutil
+import invoke
 import pathlib
 
 import utils
 import utils.doc_creator
 
-PROJECT_NAME = 'Держатель-Для-Сварочной-Горелки'
-PROJECT_PATH = pathlib.Path(__file__).with_name(f'{PROJECT_NAME}-Сборка.SLDASM')
+PROJECT_NAME = 'Верстак-Dim1000x600x50'
+PROJECT_PATH = pathlib.Path(__file__).with_name(f'{PROJECT_NAME}.SLDPRT')
 DOC_FOLDER = PROJECT_PATH.with_name(f'{PROJECT_NAME}-DOC')
-
+    
 
 @utils.sw_task(doc_string=f"Clear folder with '{PROJECT_NAME}'-project documentation")
 def clear_doc_folder(ctx):
@@ -35,32 +35,11 @@ def prepare_manufacturing_doc(ctx):
 
         td_preparator = utils.doc_creator.CNCLaserCuttingDocCreator.TableDataPreparator(
             saving_groups, manufacturing_doc_folder, lambda expression, component_full_name: bool(re.match(f"{PROJECT_NAME} {expression}", component_full_name)))
+
+            # .add_6mm_steel_sheet_table(td_preparator.prepare([f".+-6мм.+"], step=True, dxf=True)) \
         utils.doc_creator.CNCLaserCuttingDocCreator(PROJECT_NAME) \
-            .add_4mm_steel_sheet_table(td_preparator.prepare([f"(основание|держатель) .+"], step=True, dxf=True)) \
+            .add_4mm_steel_sheet_table(td_preparator.prepare([f"крепёжный-уголок"], step=True, dxf=True, quantity_expression=lambda x: 16)) \
             .create(manufacturing_doc_folder)
-
-
-@utils.sw_task(doc_string=f"Prepare assembling documentation for the project '{PROJECT_NAME}'", pre=[parse_saving_groups])
-def prepare_assembling_doc(ctx):
-    saving_groups = getattr(ctx, 'saving_groups')
-    execute = True
-    if execute:
-        assembling_doc_folder = DOC_FOLDER / 'Assembling'
-        shutil.rmtree(assembling_doc_folder, ignore_errors=True)
-
-        special_elements_table = utils.doc_creator.SpecialElementsAssemblyTable(saving_groups)
-        special_elements_table.prepare_data([f"{PROJECT_NAME} магнит"],
-                                            step=True,
-                                            save_folder_opt=assembling_doc_folder,
-                                            buy_link='https://ozon.ru/t/7GG68ot')
-
-        iso_elements_table = utils.doc_creator.ISOToolboxAssemblyTable(saving_groups)
-        iso_elements_table.prepare_data()
-
-        utils.doc_creator.AssemblyDocCreator(PROJECT_NAME) \
-            .add_table("Магниты", special_elements_table) \
-            .add_table("ISO-Крепёж", iso_elements_table) \
-            .create(assembling_doc_folder)
 
 
 @utils.sw_task(doc_string=f"Wrapping documentation for the project '{PROJECT_NAME}' to ZIP archive")
@@ -70,7 +49,7 @@ def convert_doc_to_zip(ctx):
 
 
 @utils.sw_task(doc_string=f"Make complex documentation for '{PROJECT_NAME}'",
-               pre=[clear_doc_folder, parse_saving_groups, prepare_manufacturing_doc, prepare_assembling_doc, convert_doc_to_zip])
+               pre=[clear_doc_folder, parse_saving_groups, prepare_manufacturing_doc, convert_doc_to_zip])
 def make_doc(ctx):
     saving_groups = getattr(ctx, 'saving_groups')
     for saving_group in saving_groups:
@@ -82,6 +61,7 @@ collection = invoke.Collection()
 collection.add_task(clear_doc_folder, name="clear-doc-folder")
 collection.add_task(parse_saving_groups, name="parse-saving-groups")
 collection.add_task(prepare_manufacturing_doc, name="prepare-manufacturing-doc")
-collection.add_task(prepare_assembling_doc, name="prepare-assembling-doc")
 collection.add_task(convert_doc_to_zip, name="convert-doc-to-zip")
 collection.add_task(make_doc, name="make-doc")
+
+
