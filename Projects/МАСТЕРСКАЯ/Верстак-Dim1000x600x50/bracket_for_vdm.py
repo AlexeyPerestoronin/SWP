298a71811714669b960a14aa6963d4d136b2db9e
Alexey Perestoronin <Alexey.Perestoronin@yandex.ru>
2026-08-06 18:58:43 +0600

    Иженерная-Стенка спрва: проектирование-5

--- Projects/МАСТЕРСКАЯ/Верстак-Dim1000x600x50/bracket_for_vdm.py
+++ Projects/МАСТЕРСКАЯ/Верстак-Dim1000x600x50/bracket_for_vdm.py
@@ -1,4 +1,3 @@
-import re
 import shutil
 import invoke
 import pathlib
---
+++ Projects/МАСТЕРСКАЯ/Инженерная-Стенка/V4/engineering_wall_ls.py
@@ -0,0 +1,147 @@
+import shutil
+import invoke
+import pathlib
+
+import utils
+import utils.doc_creator
+
+import utils
+import check
+
+
+PROJECT_NAME = 'Верстак-Dim1000x600x50'
+CONFIGURATION_NAME = None
+QUANTITY_EVALUATOR = lambda x: x
+PROJECT_PATH = pathlib.Path(__file__).with_name(f'{PROJECT_NAME}.SLDPRT')
+DOC_FOLDER = PROJECT_PATH.with_name(f'{PROJECT_NAME} Кронштейн-Для-ВСС DOC-x{QUANTITY_EVALUATOR(1)}')
+
+
+@utils.sw_task(doc_string=f"Clear folder with '{PROJECT_NAME}'-project documentation")
+def clear_doc_folder(ctx):
+    shutil.rmtree(DOC_FOLDER, ignore_errors=True)
+
+
+@utils.sw_task(doc_string=f"Prepare solid-bodies-saving-groups for '{PROJECT_NAME}'-project")
+def parse_saving_groups(ctx):
+    if not hasattr(ctx, 'saving_groups'):
+        unique_bodies_manager = utils.UniqueBodiesManager()
+        unique_bodies_manager.add_from_project(PROJECT_PATH, configuration=CONFIGURATION_NAME)
+        saving_groups = utils.prepare_saving_groups(unique_bodies_manager.unique_bodies)
+        setattr(ctx, 'saving_groups', saving_groups)
+
+
+@utils.sw_task(doc_string=f"Prepare steel-manufacturing documentation for the project '{PROJECT_NAME}'", pre=[parse_saving_groups])
+def prepare_steel_manufacturing_doc(ctx):
+    saving_groups = getattr(ctx, 'saving_groups')
+    execute = True
+    if execute:
+        steel_manufacturing_doc_folder = DOC_FOLDER / 'Steel' / 'Manufacturing'
+        shutil.rmtree(steel_manufacturing_doc_folder, ignore_errors=True)
+
+        # if try_detect(r"Гаражный-Комплекс.+?") \
+        # or try_detect(r"Верстак-Dim1000x600x50 столешница") :
+        #     utils.info.log_line(f"detected DOC-unused step-file: {step_path}")
+        #     continue
+
+        # # profile_tube_50_25_2
+        # elif try_detect(r"Опорная-Колонна каркас-полочек.+?") \
+        #   or try_detect(r"Профильный-Каркас Крепёжная-Планка планка-(левая|правая)"):
+        #     profile_tube_50_25_2.append([component_full_name, quantity])
+
+        # # profile_tube_50_50_4
+        # elif try_detect(r"Опорная-Колонна каркас (колонна|каркас-основания-(I|II|III))-(R|L)") \
+        #   or try_detect(r"Перфорированная-Стяжка стяжка-\d+"):
+        #     profile_tube_50_50_4.append([component_full_name, quantity])
+
+        # elif try_detect(r"Инженерная-Стенка Горизонтальный-Каркас г-опора"):
+        #     profile_tube_50_50_4.append([component_full_name, quantity])
+
+        # # steel_sheet_4
+        # elif try_detect(r"Опорная-Колонна кронштейн-потолочной-балки") \
+        #   or try_detect(r"Опорная-Колонна кк-горизонтальной-балки") \
+        #   or try_detect(r"Профильный-Каркас Крепёжная-Планка крепёжное-окно") \
+        #   or try_detect(r"Перфорированная-Стяжка ушко"):
+        #     steel_sheet_4.append([component_full_name, quantity])
+
+        # elif try_detect(r"Опорная-Колонна кк-полочки-верхний"):
+        #     # 5 (полочек для опоры) * 4 (опоры на верстаке) * 2 (верстака) = 40
+        #     steel_sheet_4.append([component_full_name, 40])
+
+        # elif try_detect(r"Опорная-Колонна кк-полочки-нижний"):
+        #     # 5 (полочек на опоре) * 4 (опоры на верстаке) * 2 (верстака) = 40
+        #     steel_sheet_4.append([component_full_name, 40])
+
+        # # steel_sheet_6
+        # elif try_detect(r"Верстак-Dim1000x600x50 т-элемент-6мм-3x3"):
+        #     steel_sheet_6.append([component_full_name, 10])
+
+        # elif try_detect(r"Верстак-Dim1000x600x50 уголок-6мм-3x3"):
+        #     steel_sheet_6.append([component_full_name, 10])
+
+        # elif try_detect(r"Верстак-Dim1000x600x50 квадрат-6мм-2x2"):
+        #     steel_sheet_6.append([component_full_name, 10])
+
+        steel_6mm = utils.doc_creator.LaserCuttingManufacturingElementsTable(saving_groups)
+        steel_6mm.prepare_data([f"{PROJECT_NAME} .+"],
+                               step=True,
+                               dxf=False,
+                               save_folder_opt=steel_manufacturing_doc_folder,
+                               quantity_evaluator=QUANTITY_EVALUATOR)
+
+        utils.doc_creator.CNCLaserCuttingDocCreator(f"Кронштейн для ВСС") \
+            .add_6mm_steel_sheet_table(steel_6mm) \
+            .create(steel_manufacturing_doc_folder)
+
+# @utils.sw_task(doc_string=f"Prepare wood-manufacturing documentation for the project '{PROJECT_NAME}'", pre=[parse_saving_groups])
+# def prepare_wood_manufacturing_doc(ctx):
+#     saving_groups = getattr(ctx, 'saving_groups')
+#     execute = True
+#     if execute:
+#         manufacturing_doc_folder = DOC_FOLDER / 'Manufacturing'
+#         shutil.rmtree(manufacturing_doc_folder, ignore_errors=True)
+
+#         class RemoveProjectPrefix(utils.doc_creator.NameTransformator):
+
+#             def __call__(self, name: str) -> str:
+#                 return name.removeprefix(f"{PROJECT_NAME} ")
+
+#         steel_6mm = utils.doc_creator.LaserCuttingManufacturingElementsTable(saving_groups)
+#         steel_6mm.prepare_data([f"{PROJECT_NAME} кронштейн-для-ВСС к-всс-.+"],
+#                                step=True,
+#                                dxf=True,
+#                                save_folder_opt=manufacturing_doc_folder,
+#                                quantity_evaluator=QUANTITY_EVALUATOR,
+#                                name_transformator=RemoveProjectPrefix())
+
+#         utils.doc_creator.CNCLaserCuttingDocCreator(f"Кронштейн для ВСС") \
+#             .add_6mm_steel_sheet_table(steel_6mm) \
+#             .create(manufacturing_doc_folder)
+
+
+@utils.sw_task(doc_string=f"Wrapping documentation for the project '{PROJECT_NAME}' to ZIP archive")
+def convert_doc_to_zip(ctx):
+    DOC_FOLDER.with_suffix('.zip').unlink(missing_ok=True)
+    shutil.make_archive(base_name=DOC_FOLDER, root_dir=DOC_FOLDER, format='zip')
+
+
+@utils.sw_task(doc_string=f"Make complex documentation for '{PROJECT_NAME}'", pre=[clear_doc_folder, parse_saving_groups, prepare_steel_manufacturing_doc, convert_doc_to_zip])
+def make_doc(ctx):
+    saving_groups = getattr(ctx, 'saving_groups')
+    for saving_group in saving_groups:
+        if saving_group.mark is None:
+            save_file_name = str(saving_group.save_file_name)
+            unused_elements = [
+                # past there names of unused elements
+            ]
+            if save_file_name in unused_elements:
+                utils.logger.info.log_line(f"detected unused elements '{save_file_name}'")
+            else:
+                utils.logger.error.log_line(f"detected unclassified elements '{save_file_name}'")
+
+
+collection = invoke.Collection()
+collection.add_task(clear_doc_folder, name="clear-doc-folder")
+collection.add_task(parse_saving_groups, name="parse-saving-groups")
+collection.add_task(prepare_steel_manufacturing_doc, name="prepare-steel-manufacturing-doc")
+collection.add_task(convert_doc_to_zip, name="convert-doc-to-zip")
+collection.add_task(make_doc, name="make-doc")
