b1fdb5c869e2396806d4ab3ee2cfa2700d727f6c
Alexey Perestoronin <Alexey.Perestoronin@yandex.ru>
2026-08-20 20:06:13 +0600

    update doc

--- Projects/МАСТЕРСКАЯ/Инженерная-Стенка/V4/engineering_wall_ls.py
+++ Projects/МАСТЕРСКАЯ/Инженерная-Стенка/V4/engineering_wall_ls.py
@@ -9,41 +9,34 @@ import utils
 import utils.doc_creator
 
 PROJECT_NAME = 'Мастерская-V4'
-PROJECT_PATH = pathlib.Path(__file__).with_name(f'{PROJECT_NAME}.SLDASM')
-DOC_FOLDER = PROJECT_PATH.parent / 'DOC' / 'Инженерная-Стенка (левая сторона)'
+PROJECT_DIR = pathlib.Path(__file__)
+DOC_FOLDER = PROJECT_DIR.parent / 'DOC' / 'Инженерная-Стенка (левая сторона)'
 
+class PassOnlyLeftSideSteelComponents(utils.AssemblyComponentsFilter):
 
-def parse_saving_groups_for_steel_doc():
-
-    class PassOnlyLeftSideSteelComponents(utils.AssemblyComponentsFilter):
-
-        def __call__(self, component: IComponent2, level: int) -> bool:
-            component_name = component.name2
-            configuration_name = component.referenced_configuration
-            is_pass = False
-            if level == 0:
-                if bool(re.match(r"^Профильный-Каркас-\d", component_name)):
-                    if configuration_name in ('левый', 'правый'):
-                        is_pass = True
-                if bool(re.match(r"^Инженерная-Стенка-ЛС-\d", component_name)):
+    def __call__(self, component: IComponent2, level: int) -> bool:
+        component_name = component.name2
+        configuration_name = component.referenced_configuration
+        is_pass = False
+        if level == 0:
+            if bool(re.match(r"^Профильный-Каркас-\d", component_name)):
+                if configuration_name in ('левый', 'правый'):
                     is_pass = True
-            else:
-                is_pass = not bool(re.search(r"Верстак-Dim1000x600x50", component_name))
+            if bool(re.match(r"^Инженерная-Стенка-ЛС-\d", component_name)):
+                is_pass = True
+        else:
+            is_pass = not bool(re.search(r"Верстак-Dim1000x600x50", component_name))
 
-            if is_pass:
-                utils.logger.info.log_line(f"{level}-level assembly component '{component_name} ({configuration_name})' passed")
-            else:
-                utils.logger.warning.log_line(f"{level}-level assembly component-'{component_name} ({configuration_name})' NOT passed")
-            return is_pass
-
-    unique_bodies_manager = utils.UniqueBodiesManager()
-    unique_bodies_manager.add_from_project(PROJECT_PATH, component_filter=PassOnlyLeftSideSteelComponents())
-    return utils.prepare_saving_groups(unique_bodies_manager.unique_bodies)
+        if is_pass:
+            utils.logger.info.log_line(f"{level}-level assembly component '{component_name} ({configuration_name})' passed")
+        else:
+            utils.logger.warning.log_line(f"{level}-level assembly component-'{component_name} ({configuration_name})' NOT passed")
+        return is_pass
 
 
 @utils.sw_task(doc_string=f"Prepare steel-manufacturing documentation for the project '{PROJECT_NAME}'")
 def prepare_steel_manufacturing_doc(ctx):
-    saving_groups = parse_saving_groups_for_steel_doc()
+    saving_groups = utils.prepare_saving_groups_for_project(PROJECT_DIR.with_name('Мастерская-V4.SLDASM'), PassOnlyLeftSideSteelComponents())
 
     steel_manufacturing_doc_folder = DOC_FOLDER / 'Manufacturing' / 'Steel'
     shutil.rmtree(steel_manufacturing_doc_folder, ignore_errors=True)
@@ -84,38 +77,17 @@ def prepare_steel_manufacturing_doc(ctx):
         .add_4mm_steel_sheet_table(steel_sheet_4mm) \
         .create(steel_manufacturing_doc_folder)
 
-    # TODO: need to check 'saving_group' for unused/unclassified elements
-    utils.prepare_archive(root_dir=steel_manufacturing_doc_folder, archive_dir=DOC_FOLDER.parent, archive_name=f"ТЗ на производство металлических деталей для инженерной стенки с левой стороны", archive_type='zip', add_date=True)
-
-
-def parse_saving_groups_for_wood_doc():
-
-    class PassOnlyLeftSideWoodComponents(utils.AssemblyComponentsFilter):
-
-        def __call__(self, component: IComponent2, level: int) -> bool:
-            component_name = component.name2
-            configuration_name = component.referenced_configuration
-            is_pass = False
-            if level == 0:
-                if bool(re.match(r"^Деревянные-Конструкции-ЛС-\d", component_name)):
-                    is_pass = True
-            else:
-                is_pass = not bool(re.search(r"Верстак-Dim1000x600x50", component_name))
-
-            if is_pass:
-                utils.logger.info.log_line(f"{level}-level assembly component '{component_name} ({configuration_name})' passed")
-            else:
-                utils.logger.warning.log_line(f"{level}-level assembly component-'{component_name} ({configuration_name})' NOT passed")
-            return is_pass
-
-    unique_bodies_manager = utils.UniqueBodiesManager()
-    unique_bodies_manager.add_from_project(PROJECT_PATH, component_filter=PassOnlyLeftSideWoodComponents())
-    return utils.prepare_saving_groups(unique_bodies_manager.unique_bodies)
+    assert utils.scan_unused_saving_group(saving_groups)
+    assert utils.prepare_archive(root_dir=steel_manufacturing_doc_folder,
+                                 archive_dir=DOC_FOLDER.parent,
+                                 archive_name=f"ТЗ на производство металлических деталей для инженерной стенки с левой стороны",
+                                 archive_type='zip',
+                                 add_date=True)
 
 
 @utils.sw_task(doc_string=f"Prepare wood-manufacturing documentation for the project '{PROJECT_NAME}'")
 def prepare_wood_manufacturing_doc(ctx):
-    saving_groups = parse_saving_groups_for_wood_doc()
+    saving_groups = utils.prepare_saving_groups_for_project(PROJECT_DIR.with_name('Деревянные-Конструкции-ЛС.SLDPRT'))
 
     wood_manufacturing_doc_folder = DOC_FOLDER / 'Manufacturing' / 'Wood'
     shutil.rmtree(wood_manufacturing_doc_folder, ignore_errors=True)
@@ -129,8 +101,8 @@ def prepare_wood_manufacturing_doc(ctx):
         ["размер", "2500x1250мм"],
     ])
 
-    obs_sheet_15mm = utils.doc_creator.StandardElementsTable(saving_groups)
-    obs_sheet_15mm.prepare_data([r"Деревянные-Конструкции-ЛС полочки.+"], step=True, dxf=True, save_folder_opt=wood_manufacturing_doc_folder)
+    osb_sheet_15mm = utils.doc_creator.StandardElementsTable(saving_groups)
+    osb_sheet_15mm.prepare_data([r"Деревянные-Конструкции-ЛС полочки.+"], step=True, dxf=True, save_folder_opt=wood_manufacturing_doc_folder)
     osb_sheet_15mm_material_info = utils.doc_creator.MaterialInfoTable()
     osb_sheet_15mm_material_info.prepare_data([
         ["материал", "OBS"],
@@ -140,11 +112,15 @@ def prepare_wood_manufacturing_doc(ctx):
 
     utils.doc_creator.CNCWoodMillingCuttingDocCreator(f"Инженерная Стенка") \
         .add_table('OBS плита 12мм', osb_sheet_12mm, osb_sheet_12mm_material_info) \
-        .add_table('OBS плита 15мм', obs_sheet_15mm, osb_sheet_15mm_material_info) \
+        .add_table('OBS плита 15мм', osb_sheet_15mm, osb_sheet_15mm_material_info) \
         .create(wood_manufacturing_doc_folder)
 
-    # TODO: need to check 'saving_group' for unused/unclassified elements
-    utils.prepare_archive(root_dir=wood_manufacturing_doc_folder, archive_dir=DOC_FOLDER.parent, archive_name=f"ТЗ на производство деревянных деталей для инженерной стенки с левой стороны", archive_type='zip', add_date=True)
+    assert utils.scan_unused_saving_group(saving_groups)
+    assert utils.prepare_archive(root_dir=wood_manufacturing_doc_folder,
+                                 archive_dir=DOC_FOLDER.parent,
+                                 archive_name=f"ТЗ на производство деревянных деталей для инженерной стенки с левой стороны",
+                                 archive_type='zip',
+                                 add_date=True)
 
 
 @utils.sw_task(doc_string=f"Make full documentation for '{PROJECT_NAME}'")
