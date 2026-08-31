81f49eb954a2ff6ae3a3141d0c049d827f96821b
Alexey Perestoronin <Alexey.Perestoronin@yandex.ru>
2026-08-31 11:51:42 +0600

    Монополия: проектирование-3

--- Projects/РАЗНОЕ/Монополия/monopoly.py
+++ Projects/РАЗНОЕ/Монополия/monopoly.py
@@ -12,39 +12,34 @@ PROJECT_NAME = 'Монополия'
 PROJECT_DIR = pathlib.Path(__file__)
 DOC_FOLDER = PROJECT_DIR.parent / 'DOC' / PROJECT_NAME
 
+@utils.sw_task(doc_string=f"Prepare wood-manufacturing documentation for the project '{PROJECT_NAME}'")
+def prepare_wood_doc(ctx):
+    saving_groups = utils.prepare_saving_groups_for_project(PROJECT_DIR.with_name(f"Карта.SLDPRT"))
 
-class PassAllExceptFastenersComponents(utils.AssemblyComponentsFilter):
-
-    def __call__(self, component: IComponent2, level: int) -> bool:
-        component_name = component.name2
-        configuration_name = component.referenced_configuration
-        is_pass = False
-        if level == 0:
-            is_pass = bool(re.match(r"Игровая-Платформа", component_name))
-        else:
-            raise Exception(f"detected component of unexpected level: {component_name}")
-
-        if is_pass:
-            utils.logger.info.log_line(f"{level}-level assembly component '{component_name} ({configuration_name})' passed")
-        else:
-            utils.logger.warning.log_line(f"{level}-level assembly component-'{component_name} ({configuration_name})' NOT passed")
-        return is_pass
-
-
-@utils.sw_task(doc_string=f"Prepare steel-manufacturing documentation for the project '{PROJECT_NAME}'")
-def prepare_platform_doc(ctx):
-    saving_groups = utils.prepare_saving_groups_for_project(PROJECT_DIR.with_name(f"{PROJECT_NAME}.SLDASM"), PassAllExceptFastenersComponents())
-
-    doc_folder = DOC_FOLDER
+    doc_folder = DOC_FOLDER / 'Manufacturing' / 'Wood'
     shutil.rmtree(doc_folder, ignore_errors=True)
 
-    pla_filament = utils.doc_creator.StandardElementsTable(saving_groups)
-    pla_filament.prepare_data([f"Игровая-Платформа база"], step=True, dxf=False, save_folder_opt=doc_folder)
+    polywood = utils.doc_creator.StandardElementsTable(saving_groups)
+    polywood.prepare_data([r"Карта (путь-внешний|путь-внутренний|круг)"], step=True, dxf=False, save_folder_opt=doc_folder)
+    polywood_material_info = utils.doc_creator.MaterialInfoTable()
+    polywood_material_info.prepare_data([
+        ["материал", "фанера"],
+        ["толщина", "12мм"],
+        ["сорт", "1 (первый)"],
+        ["наличие ламинирования", "допускается, но только двустороннее"],
+        ["влагозащита", "допускается, но не обязательно"],
+    ])
 
-    utils.doc_creator.CNC3DPrintingDocCreator(f"Монополия") \
-        .add_PLA(pla_filament) \
+    utils.doc_creator.CNCWoodMillingDocCreator(f"Монополия") \
+        .add_table('Фанера 12мм', polywood, polywood_material_info) \
         .create(doc_folder)
+
+    assert utils.prepare_archive(root_dir=doc_folder,
+                                 archive_dir=DOC_FOLDER.parent,
+                                 archive_name=f"ТЗ на производство деревянных деталей для монополии",
+                                 archive_type='zip',
+                                 add_date=True)
 
 
 collection = invoke.Collection()
-collection.add_task(prepare_platform_doc)
+collection.add_task(prepare_wood_doc)
